deed8b515432ea1a23db1b293109350eaed900ce
VarjoDesigns <57039872+VarjoDesigns@users.noreply.github.com>
2021-09-22 16:56:12 +0300

    Project initialization
    Setting up a project

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>React portfolio application</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Varjo Designs website in single page form. Coded with React and published on github. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
